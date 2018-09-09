4bf82b2fee6406a494bd9dd5a4cd976af4add281
liangze <249834096@qq.com>
2018-09-09 16:38:55 +0800

    update data processor

--- core/data_processor.py
+++ core/data_processor.py
@@ -6,10 +6,11 @@ class DataLoader():
 	"""A class for loading and transforming data for the lstm model"""
 
 	def __init__(self, filename, split, cols):
-		dataframe = pd.read_csv(filename)
-		i_split = int(len(dataframe) * split)
-		self.data_train = dataframe.get(cols).values[:i_split]
-		self.data_test  = dataframe.get(cols).values[i_split:]
+		df = pd.read_csv(filename).get(cols)
+		df = df[df.values != 0]
+		i_split = int(len(df) * split)
+		self.data_train = df.get(cols).values[:i_split]
+		self.data_test  = df.get(cols).values[i_split:]
 		self.len_train  = len(self.data_train)
 		self.len_test   = len(self.data_test)
 		self.len_train_windows = None
@@ -23,13 +24,16 @@ class DataLoader():
 		data_windows = []
 		for i in range(self.len_test - seq_len):
 			data_windows.append(self.data_test[i:i+seq_len])
-
 		data_windows = np.array(data_windows).astype(float)
 		data_windows = self.normalise_windows(data_windows, single_window=False) if normalise else data_windows
 
 		x = data_windows[:, :-1]
-		y = data_windows[:, -1]
+		# one dim
+		# y = data_windows[:, -1]
+		# two dim
+		y = data_windows[:, -1, [0]]
 		#x = x.reshape(1, x.shape[0], x.shape[1])
+
 		return x,y
 
 	def get_train_data(self, seq_len, normalise):
@@ -44,6 +48,7 @@ class DataLoader():
 			x, y = self._next_window(i, seq_len, normalise)
 			data_x.append(x)
 			data_y.append(y)
+
 		return np.array(data_x), np.array(data_y)
 
 	def generate_train_batch(self, seq_len, batch_size, normalise):
@@ -67,7 +72,10 @@ class DataLoader():
 		window = self.data_train[i:i+seq_len]
 		window = self.normalise_windows(window, single_window=True)[0] if normalise else window
 		x = window[:-1]
-		y = window[-1]
+		# one dim
+		# y = window[-1]
+		# two dim
+		y = window[-1, [0]]
 		return x, y
 
 	def normalise_windows(self, window_data, single_window=False):
@@ -75,7 +83,14 @@ class DataLoader():
 		normalised_data = []
 		window_data = [window_data] if single_window else window_data
 		for window in window_data:
-			normalised_window = [((float(p) / float(window[0])) - 1) for p in window]
-			normalised_window = np.reshape(normalised_window, (len(normalised_window), 1))
+			# one dim
+			# normalised_window = [((float(p) / float(window[0])) - 1) for p in window]
+			# normalised_window = np.reshape(normalised_window, (len(normalised_window), 1))
+			# two dim
+			normalised_window = []
+			for col_i in range(window.shape[1]):
+				normalised_col = [((float(p) / float(window[0, col_i])) - 1) for p in window[:, col_i]]
+				normalised_window.append(normalised_col)
+			normalised_window = np.array(normalised_window).T  # reshape and transpose array back into original multidimensional format
 			normalised_data.append(normalised_window)
 		return np.array(normalised_data)
